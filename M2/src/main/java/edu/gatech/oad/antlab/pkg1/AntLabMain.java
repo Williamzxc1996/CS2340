c9ecb0cbf5c4eb7b8ed6de7d0680d42e1066308f
xzhou364 <zhouxuncheng@lawn-143-215-124-86.lawn.gatech.edu>
2019-09-11 20:19:19 -0400

    Replace Person1 arguments in AntLabMain

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -57,8 +57,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("P1");
-		  toPrint += p1.toString("gburdell1");
+		  Person1 p1 = new Person1("XunchengZhou");
+		  toPrint += p1.toString("903423628");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("P2");
