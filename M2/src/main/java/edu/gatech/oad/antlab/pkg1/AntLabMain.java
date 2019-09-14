ef913e2de57f83eb0d5f3ef915652333f1c8e6b9
frank-gatech <xzhong66@gatech.edu>
2019-09-14 10:44:51 -0400

    person5 modified antlab

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -62,19 +62,19 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("Zhiquan");
-		  toPrint += p2.toString("201400171049");
+		  toPrint += p2.toString("903427730");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Qianzhen Li");
-		  toPrint += p3.toString("qli365");
+		  toPrint += p3.toString("903361619");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+                  Person4 p4 = new Person4("Xiaoyou Jiang");
+                  toPrint += p4.toString("903427782");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("XiangZhong");
+                  toPrint += p5.toString("903526232");
 		  
         System.out.println(toPrint);
         
